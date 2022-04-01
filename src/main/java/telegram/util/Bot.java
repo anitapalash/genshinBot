1e9463c1f06ea84d63e14e842e1e2f7f7260fa6a
anitapalash <Sergey4e6ir>
2022-04-01 22:44:23 +0300

    init commit

---
+++ src/main/java/telegram/util/Bot.java
@@ -0,0 +1,76 @@
+package telegram.util;
+
+import telegram.util.command.StartCommand;
+import org.telegram.telegrambots.extensions.bots.commandbot.TelegramLongPollingCommandBot;
+import org.telegram.telegrambots.extensions.bots.commandbot.commands.helpCommand.HelpCommand;
+import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
+import org.telegram.telegrambots.meta.api.objects.Message;
+import org.telegram.telegrambots.meta.api.objects.Update;
+import org.telegram.telegrambots.meta.api.objects.User;
+import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
+
+public class Bot extends TelegramLongPollingCommandBot {
+    private final String BOT_NAME;
+    private final String BOT_TOKEN;
+
+    public Bot(String botName, String botToken) {
+        super();
+        this.BOT_NAME = botName;
+        this.BOT_TOKEN = botToken;
+        //создаём вспомогательный класс для работы с сообщениями, не являющимися командами
+        //регистрируем команды
+        register(new StartCommand("start", "Старт"));
+        register(new HelpCommand("help","Помощь", "Перечисление всех " +
+                "доступных команд для исполнения"));
+    }
+
+    @Override
+    public String getBotToken() {
+        return BOT_TOKEN;
+    }
+
+    @Override
+    public String getBotUsername() {
+        return BOT_NAME;
+    }
+
+    /**
+     * Ответ на запрос, не являющийся командой
+     */
+    @Override
+    public void processNonCommandUpdate(Update update) {
+        Message msg = update.getMessage();
+        Long chatId = msg.getChatId();
+        String userName = getUserName(msg);
+
+//        String answer = nonCommand.nonCommandExecute(chatId, userName, msg.getText());
+//        setAnswer(chatId, userName, answer);
+    }
+
+    /**
+     * Формирование имени пользователя
+     * @param msg сообщение
+     */
+    private String getUserName(Message msg) {
+        User user = msg.getFrom();
+        String userName = user.getUserName();
+        return (userName != null) ? userName : String.format("%s %s", user.getLastName(), user.getFirstName());
+    }
+
+    /**
+     * Отправка ответа
+     * @param chatId id чата
+     * @param userName имя пользователя
+     * @param text текст ответа
+     */
+    private void setAnswer(Long chatId, String userName, String text) {
+        SendMessage answer = new SendMessage();
+        answer.setText(text);
+        answer.setChatId(chatId.toString());
+        try {
+            execute(answer);
+        } catch (TelegramApiException e) {
+            //логируем сбой Telegram telegram.util.Bot API, используя userName
+        }
+    }
+}
